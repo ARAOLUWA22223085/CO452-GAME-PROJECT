75995b63b768b58cd8ed27a0bf712387161acfe5
ARAOLUWA22223085 <114756077+ARAOLUWA22223085@users.noreply.github.com>
2022-12-22 00:20:50 +0000

    final update, pacman has been animated and faces the direction of the direction of travel! project complete!

--- index.js
+++ index.js
@@ -29,20 +29,35 @@ class Player {
         this.position = position;
         this.velocity = velocity;
         this.radius = 15;
+        this.radians = 0.75
+        this.openRate = 0.12
+        this.rotation = 0
     }
 
     draw() {
+        c.save()
+        c.translate(this.position.x, this.position.y)
+        c.rotate(this.rotation)
+        c.translate(-this.position.x, -this.position.y)
+
         c.beginPath()
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.arc(this.position.x, this.position.y, this.radius, this.radians , Math.PI * 2 - this.radians)
+        c.lineTo (this.position.x, this.position.y)
         c.fillStyle = 'yellow';
         c.fill()
         c.closePath
+        c.restore()
     }
 
     update() {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
+
+        if (this.radians < 0 || this.radians > 0.75)
+            this. openRate = -this.openRate
+            this.radians += this.openRate
+        
     }
 }
 
@@ -356,8 +371,12 @@ function animate() {
                 cancelAnimationFrame(animationId)
                 console.log('you lose. Loser.')
             }
-
         }
+    }
+
+    if(pellets.length === 0 || ghosts.length === 0){
+        cancelAnimationFrame(animationId)
+        console.log('Oh wow, you actually won? Good job Chump.')
     }
 
     for (let i = powerUps.length - 1; 0 <= i; i--) {
@@ -519,6 +538,11 @@ function animate() {
             ghost.prevCollisions = []
         }
     })
+
+    if(player.velocity.x > 0) player.rotation = 0
+    else if(player.velocity.x < 0) player.rotation = Math.PI
+    else if(player.velocity.y > 0) player.rotation = Math.PI / 2
+    else if(player.velocity.y < 0) player.rotation = Math.PI *1.5
 
 }
 
